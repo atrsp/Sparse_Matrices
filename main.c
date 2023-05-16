2a21e267b9db20e2ea583fbc2ccc1e31849ce6ad
atrsp <atrspereira@gmail.com>
2023-05-16 18:56:13 -0300

    Insert and Destroy Functions completed

--- main.c
+++ main.c
@@ -4,26 +4,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SZ_LINES 4
-#define SZ_COLUMNS 7
+#define SZ_LINES 20
+#define SZ_COLUMNS 20
 
 int main (){
 
     Matrix* m = construct_matrix (SZ_LINES, SZ_COLUMNS);
-
-    add_value_matrix(m, 2, 3, 1);
-    print_dense_matrix (m);
-
-    printf ("\n\n");
-
-    add_value_matrix(m, 2, 3, 4);
-    print_dense_matrix (m);
-
-    printf ("\n\n");
-
-    add_value_matrix(m, 2, 4, 5);
-    add_value_matrix(m, 2, 6, 8);
-    add_value_matrix(m, 2, 5, 9);
 
     for (int i=0;i<SZ_LINES;i++){
         for (int j=0;j<SZ_COLUMNS; j++)
@@ -32,7 +18,7 @@ int main (){
         
     print_dense_matrix (m);
 
-    //print_sparse_matrix (m);
+    print_sparse_matrix (m);
 
     destroy_matrix (m);
 
--- matrix.c
+++ matrix.c
@@ -22,22 +22,20 @@ Matrix* construct_matrix (int sz_lines, int sz_columns)
 
 void destroy_matrix (Matrix* m)
 {
-    
-    Cell* prev = NULL;
 
-    for (int i=0; i< m->size_columns; i++)
+    for (int i = 0; i < m->size_lines; i++)
     {   
-        Cell* c = m->head_columns[i];
+        Cell* c = m->head_lines[i];
+        Cell* prev = NULL;
 
-        while (c!= NULL)
+        while (c != NULL)
         {   
-            prev = c;
-            free (c);
-            c = prev->next_line;
+            prev = c->next_column;
+            destroy_cell(c);
+            c = prev;
         }
     }
     
-
     free (m->head_lines);
     free (m->head_columns);
     free (m);
@@ -51,47 +49,107 @@ void destroy_matrix (Matrix* m)
 void add_value_matrix(Matrix* m, int idx_line, int idx_column, float value)
 {       
 
-    if (value != 0 && idx_line > 0 && idx_line < m->size_lines && idx_column > 0 && idx_column < m->size_columns)
+    if (value != 0 && idx_line >= 0 && idx_line < m->size_lines && idx_column >= 0 && idx_column < m->size_columns)
     {   
+
+        //por colunas
+
         Cell* aux = m->head_lines [idx_line];
         Cell* prev = NULL;
 
-        while (aux != NULL)
-        {   
-            if (aux->column == idx_column)
-            {
-                aux->value = value;
-                break;
-            }
+        Cell* c = construct_cell (idx_line, idx_column, value);
+        m->size_cells++;
 
-            if (prev != NULL)
-            {
-                if (prev->column < idx_column && aux->column > idx_column)
+        if (m->head_lines[idx_line] == NULL)
+            m->head_lines[idx_line] = c;
+        
+
+        else 
+        {
+            while (aux != NULL)
+            {   
+                if (idx_column < aux->column)
+                {
+                    if (prev != NULL)
+                    {
+                        prev->next_column = c;
+                        c->next_column = aux;
+                        break;
+                    }
+
+                    else 
+                    {
+                        prev = m->head_lines[idx_line];
+                        m->head_lines[idx_line] = c;
+                        c->next_column = prev;
+                        break;
+                    }
+
+                }
+
+                if (aux->column == idx_column)
                 {   
-                    Cell* c = construct_cell (idx_line, idx_column, value);
-                    prev->next_column = c;
-                    c->next_column = aux;
-                    m->size_cells++;
-
+                    m->size_cells--;
+                    destroy_cell (c);
+                    aux->value = value;
                     break;
                 }
+
+                prev = aux;
+                aux = aux->next_column;
             }
 
-            prev = aux;
-            aux = aux->next_column;
+            if (aux == NULL)
+                if (prev != NULL)
+                    prev->next_column = c;
+        
         }
 
-        if (aux == NULL)
+        // por linhas
+
+        aux = m->head_columns[idx_column];
+        prev = NULL;
+
+        if (m->head_columns[idx_column] == NULL)
+            m->head_columns[idx_column] = c;
+
+        else 
         {
-            Cell* c = construct_cell (idx_line, idx_column, value);
+            while (aux != NULL)
+            {   
+                if (idx_line < aux->line)
+                {
+                    if (prev != NULL)
+                    {
+                        prev->next_line = c;
+                        c->next_line = aux;
+                        m->size_cells++;
+                        break;
+                    }
 
-            if (prev != NULL)
-                prev->next_column = c;
+                    else 
+                    {
+                        prev = m->head_columns[idx_column];
+                        m->head_columns[idx_column] = c;
+                        c->next_line = prev;
+                        break;
+                    }
+                }
 
-            else
-                m->head_lines[idx_line] = c;
+                if (aux->line == idx_line)
+                {
+                    aux->value = value;
+                    break;
+                }
 
-            m->size_cells++;
+                prev = aux;
+                aux = aux->next_line;
+            }
+
+            if (aux == NULL)
+                if (prev != NULL)
+                    prev->next_line = c;
+        
         }
     }
 }
@@ -115,7 +173,8 @@ void print_sparse_matrix (Matrix* m)
 }
 
 void print_dense_matrix (Matrix* m)
-{      
+{     
+    //por colunas
     for (int i=0; i< m->size_lines; i++)
     {
         Cell* c = m->head_lines [i];
@@ -135,5 +194,30 @@ void print_dense_matrix (Matrix* m)
         printf ("\n\n");
     }
 
+    printf ("\n\n");
 
+    //por linhas
+    /*
+    for (int i=0; i< m->size_columns; i++)
+    {
+        Cell* c = m->head_columns [i];
+
+        for (int j=0; j < m->size_lines; j++)
+        {   
+            if (c != NULL && c->column == i && c->line == j)
+            {
+                printf ("%.0f  ", c->value);
+                c = c->next_line;
+            }
+            else
+                printf ("0  ");
+
+        }
+
+        printf ("\n\n");
+    }
+
+    printf ("\n\n");
+
+    */
 }
